2c18d2269fe5349cfc79c3906d060cf6994faad7
yongchane <202021113@sangmyung.kr>
2024-11-18 23:59:28 +0900

    최근 본 프로젝트 컴포넌트 구축 및 이미지 슬라이스구축, 필요없는 파일 삭제

--- src/page/main/Recent.jsx
+++ src/page/main/Recent.jsx
@@ -1,21 +1,15 @@
 import React, { useState } from "react";
+import { ReactComponent as Next } from "../../assets/component/next.svg";
+import { ReactComponent as ActiveNext } from "../../assets/component/ActiveNext.svg";
+import { ReactComponent as Back } from "../../assets/component/goback.svg";
+import { ReactComponent as ActiveBack } from "../../assets/component/Activegoback.svg";
 
-import { ReactComponent as Next } from "../../../../assets/component/BigNext.svg";
-import { ReactComponent as ActiveNext } from "../../../../assets/component/AtBigNext.svg";
-import { ReactComponent as Back } from "../../../../assets/component/BigBack.svg";
-import { ReactComponent as ActiveBack } from "../../../../assets/component/AtBigBack.svg";
-const menu = [
-  { name: "주간" },
-  { name: "월간" },
-  { name: "전체" },
-  { name: "프로젝트 수" },
-];
-
-const ComPreview = () => {
+const Recent = () => {
   const [currentIndex, setCurrentIndex] = useState(0); // 현재 슬라이드 인덱스
   const [isNextActive, setIsNextActive] = useState(false); // ActiveNext 렌더링 상태
   const [isBackActive, setIsBackActive] = useState(false); // ActiveBack 렌더링 상태
 
+  // 총 이미지 배열
   const images = [
     "https://via.placeholder.com/84x105?text=Image+1",
     "https://via.placeholder.com/84x105?text=Image+2",
@@ -45,29 +39,21 @@ const ComPreview = () => {
   };
 
   return (
-    <div className="rank-preview-container">
-      {/* 주간,월간 별로 기업 랭킹 불러오기 필요 */}
-      <div className="rank-preview-menu-container">
-        {menu.map((day) => (
-          <div className="rank-preview-menus">{day.name}</div>
-        ))}
-      </div>
-
-      {/* 기업 이미지 연동 필요 */}
-
-      <div className="rank-compant-img">
-        {/* 현재 인덱스부터 3개의 이미지를 슬라이드  추후 기업 랭킹 이미지 연동 필요*/}
+    <div className="recent-project-container">
+      <div className="recent-project-title">최근 본 프로젝트</div>
+      <div className="recent-project-imgs">
+        {/* 현재 인덱스부터 3개의 이미지를 슬라이드  추후 최근 본 이미지 연동 필요*/}
         {images
           .slice(currentIndex, currentIndex + imagesPerPage)
           .map((src, index) => (
             <div
               key={index}
-              className="rank-preview-img-container"
+              className="recent-project-img"
               style={{ backgroundImage: `url(${src})` }}
             ></div>
           ))}
       </div>
-      <div className="rank-preview-next-btn">
+      <div className="recent-project-btn">
         <div className="back-btn" onClick={handleBack}>
           {isBackActive ? <ActiveBack /> : <Back />}
         </div>
@@ -76,10 +62,8 @@ const ComPreview = () => {
           {isNextActive ? <ActiveNext /> : <Next />}
         </div>
       </div>
-
-      <div className="rank-preview-next-btn"></div>
     </div>
   );
 };
 
-export default ComPreview;
+export default Recent;
