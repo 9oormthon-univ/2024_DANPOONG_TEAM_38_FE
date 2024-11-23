29fcbe98922067073d57bdf84dc02443ced623ab
Hina0222 <hina0222@naver.com>
2024-11-24 03:11:34 +0900

    feat: logout 설정

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,11 +5,9 @@ import { ReactComponent as Fundboost } from "../assets/applogo/fundboost.svg";
 import { ReactComponent as Back } from "../assets/component/back.svg";
 import PostCompanyLogout from "../apis/login/company/PostCompanyLogout";
 import DeleteKakaoLogout from "../apis/login/DeleteKakaoLogout";
-import UpHeader from "../page/project/upload/UpHeader";
 
 const Header = () => {
   const location = useLocation();
-  const path = location.pathname;
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -26,29 +24,36 @@ const Header = () => {
     { name: "이벤트", path: "/event" },
   ];
 
-  const showUpHeader = ["/write", "/fund", "/plan", "/createuser"];
-  const showHeader = showUpHeader.includes(path);
-
   useEffect(() => {
     const token = sessionStorage.getItem("accessToken");
     setIsLoggedIn(!!token);
   }, [location.pathname]);
 
-  const isActive = (menuPath) => location.pathname === menuPath;
+  const isActive = (path) => location.pathname === path;
 
-  const goMenuHandler = (menuPath) => {
-    navigate(menuPath);
+  const goMenuHandler = (path) => {
+    navigate(path);
   };
 
   const clickLogout = async () => {
     const type = sessionStorage.getItem("type");
 
     if (type === "USER") {
-      await DeleteKakaoLogout();
+      const result = await DeleteKakaoLogout();
+      sessionStorage.removeItem("accessToken");
+      sessionStorage.removeItem("refreshToken");
+      sessionStorage.removeItem("type");
+      alert("로그아웃 하였습니다.");
+      setIsLoggedIn(false);
+      navigate("/");
     } else if (type === "COMPANY") {
       const result = await PostCompanyLogout();
+      console.log(result);
       if (result?.status === 200) {
-        sessionStorage.clear();
+        sessionStorage.removeItem("accessToken");
+        sessionStorage.removeItem("refreshToken");
+        sessionStorage.removeItem("email");
+        sessionStorage.removeItem("type");
         alert("로그아웃 하였습니다.");
         setIsLoggedIn(false);
         navigate("/");
@@ -57,49 +62,50 @@ const Header = () => {
   };
 
   return (
-    <div className="header-container">
-      <div className="header-title-container">
-        <div className="header-main" onClick={() => navigate("/")}>
-          <Fundboost />
-        </div>
+      <div className="header-container">
+        <div className="header-title-container">
+          <div className="header-main" onClick={() => navigate("/")}>
+            <Fundboost/>
+          </div>
 
-        <div className="header-title-list-container">
-          {(isLoggedIn ? menuok : menu).map((item) => (
-            <div
-              key={item.path}
-              className={`header-title-menu ${
-                isActive(item.path) ? "active" : ""
-              }`}
-              onClick={() => goMenuHandler(item.path)}
-            >
-              {item.name}
-            </div>
-          ))}
-        </div>
+          <div className="header-title-list-container">
+            {(isLoggedIn ? menuok : menu).map((item) => (
+                <div
+                    key={item.path}
+                    className={`header-title-menu ${
+                        isActive(item.path) ? "active" : ""
+                    }`}
+                    onClick={() => goMenuHandler(item.path)}
+                >
+                  {item.name}
+                </div>
+            ))}
+          </div>
 
-        <div className="header-login-container">
-          {isLoggedIn ? (
-            <>
-              <button
-                className="header-mypage"
-                onClick={() => navigate("/myPage")}
-              >
-                마이페이지
-              </button>
-              <button className="header-logout" onClick={clickLogout}>
-                로그아웃
-              </button>
-            </>
-          ) : (
-            <button className="header-login" onClick={() => navigate("/login")}>
-              로그인
-              <Back />
-            </button>
-          )}
+          <div className="header-login-container">
+            {isLoggedIn ? (
+                <>
+                  <button
+                      className="header-mypage"
+                      onClick={() => {
+                        navigate("/myPage");
+                      }}
+                  >
+                    마이페이지
+                  </button>
+                  <button className="header-logout" onClick={clickLogout}>
+                    로그아웃
+                  </button>
+                </>
+            ) : (
+                <button className="header-login" onClick={() => navigate("/login")}>
+                  로그인
+                  <Back/>
+                </button>
+            )}
+          </div>
         </div>
       </div>
-      {showHeader && <UpHeader />}
-    </div>
   );
 };
 
